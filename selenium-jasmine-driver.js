ba984d8acaec41c958c9e16ccfb345d8966a63fb
Tony Tsui <tony.tsui@timgroup.com>
2015-01-20 23:48:00 +0000

    Read JUnit results and write it to files.

--- selenium-jasmine-driver.js
+++ selenium-jasmine-driver.js
@@ -6,9 +6,23 @@ var driver = new webdriver.Builder().
 
 driver.get('http://localhost:8080/test/jasmine-test-runner.html');
 driver.wait(function() {
-  return driver.getTitle().then(function(title) {
-    return title === 'Jasmine Test Runner';
-  });
+  return driver.isElementPresent(webdriver.By.className('bar'));
 }, 1000);
+
+driver.executeScript('return junitResults;').then(function(junitResults) {
+    junitResults.map(function(result) {
+        var fs = require("fs");
+        var nodejs_path = require("path");
+
+        var path = result.filename.substring(0, result.filename.lastIndexOf('/'));
+        var filename = result.filename.substring(result.filename.lastIndexOf('/'));
+
+        require("mkdirp").sync(path); // make sure the path exists
+        var filepath = nodejs_path.join(path, filename);
+        var xmlfile = fs.openSync(filepath, "w");
+        fs.writeSync(xmlfile, result.content, 0);
+        fs.closeSync(xmlfile);
+    });
+});
 
 driver.quit();
